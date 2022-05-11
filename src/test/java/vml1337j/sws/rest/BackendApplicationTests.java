349ada29daeaa35689993128277250d98b8c4b28
vml1337j <vml1337j@yandex.ru>
2022-05-11 11:39:51 +0300

    init commit

---
+++ src/test/java/vml1337j/sws/rest/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vml1337j.sws.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
