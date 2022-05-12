6642888b7bec83968c58997b05a205d1616bebd3
vml1337j <vml1337j@yandex.ru>
2022-05-13 00:11:51 +0300

    add @noargsconstructor

--- src/main/java/vml1337j/sws/rest/api/dto/EventDto.java
+++ src/main/java/vml1337j/sws/rest/api/dto/EventDto.java
@@ -3,11 +3,13 @@ package vml1337j.sws.rest.api.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class EventDto {
     private long id;
--- src/main/java/vml1337j/sws/rest/api/dto/RacerDto.java
+++ src/main/java/vml1337j/sws/rest/api/dto/RacerDto.java
@@ -3,9 +3,11 @@ package vml1337j.sws.rest.api.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class RacerDto {
     private long id;
