16eb8b657b72adf385e35d0125eb985d387b7453
vml1337j <vml1337j@yandex.ru>
2022-05-13 11:46:40 +0300

    add EventMapper

--- src/main/java/vml1337j/sws/rest/api/controllers/EventController.java
+++ src/main/java/vml1337j/sws/rest/api/controllers/EventController.java
@@ -1,13 +1,17 @@
 package vml1337j.sws.rest.api.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import vml1337j.sws.rest.api.dto.EventDto;
 import vml1337j.sws.rest.api.dto.EventResultDto;
+import vml1337j.sws.rest.api.mappers.EventMapper;
+import vml1337j.sws.rest.store.entities.EventEntity;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class EventController {
 
@@ -15,18 +19,20 @@ public class EventController {
     public static final String GET_EVENT = "/api/v1/events/{event_id}";
     public static final String FETCH_EVENT_RESULTS = "/api/v1/events/{event_id}/results";
 
+    private final EventMapper eventMapper;
+
     @GetMapping(FETCH_EVENTS)
     public List<EventDto> fetchEvents() {
-        return List.of();
+        return eventMapper.toEventDtoList(List.of());
     }
 
     @GetMapping(GET_EVENT)
     public EventDto getEventById(@PathVariable("event_id") Long eventId) {
-        return EventDto.builder().build();
+        return eventMapper.toEventDto(EventEntity.builder().build());
     }
 
     @GetMapping(FETCH_EVENT_RESULTS)
     public List<EventResultDto> fetchEventResultsById(@PathVariable("event_id") Long eventId) {
-        return List.of();
+        return eventMapper.toEventResultDtoList(List.of());
     }
 }
