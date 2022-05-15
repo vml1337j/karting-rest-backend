9f4bf90cc509e1723e0dd13c2527cdba7d7eeac1
vml1337j <vml1337j@yandex.ru>
2022-05-15 23:52:23 +0300

    add racer mapper

---
+++ src/main/java/vml1337j/sws/rest/api/mappers/RacerMapper.java
@@ -0,0 +1,27 @@
+package vml1337j.sws.rest.api.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import vml1337j.sws.rest.api.dto.RacerDto;
+import vml1337j.sws.rest.api.dto.RacerEventDto;
+import vml1337j.sws.rest.store.entities.RacerEntity;
+import vml1337j.sws.rest.store.entities.ResultEntity;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RacerMapper {
+
+    RacerDto toRacerDto(RacerEntity entity);
+
+    List<RacerDto> toRacerDtoList(List<RacerEntity> entities);
+
+    List<RacerEventDto> toRacerResultDtoList(List<ResultEntity> entities);
+
+    @Mapping(source = "event.id", target = "eventId")
+    @Mapping(source = "event.title", target = "title")
+    @Mapping(source = "event.date", target = "date")
+    @Mapping(source = "event.category", target = "category")
+    RacerEventDto toRacerResultDto(ResultEntity entity);
+
+}
