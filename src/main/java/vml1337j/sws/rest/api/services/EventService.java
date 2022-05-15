669ae8befdb9d37061773d913b67f072244debb9
vml1337j <vml1337j@yandex.ru>
2022-05-15 20:56:29 +0300

    add methods for get event data

--- src/main/java/vml1337j/sws/rest/api/services/EventService.java
+++ src/main/java/vml1337j/sws/rest/api/services/EventService.java
@@ -2,11 +2,15 @@ package vml1337j.sws.rest.api.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import vml1337j.sws.rest.api.exceptions.EventNotFoundException;
 import vml1337j.sws.rest.store.entities.EventEntity;
+import vml1337j.sws.rest.store.entities.RacerEntity;
 import vml1337j.sws.rest.store.entities.ResultEntity;
 import vml1337j.sws.rest.store.repositories.EventRepository;
-import vml1337j.sws.rest.store.repositories.ResultRepository;
+import vml1337j.sws.rest.store.repositories.RacerRepository;
+import vml1337j.sws.rest.utils.SwsUtils;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,31 +19,62 @@ import java.util.stream.Collectors;
 public class EventService {
 
     private final EventRepository eventRepository;
-    private final ResultRepository resultRepository;
+    private final RacerRepository racerRepository;
+    private final SwsUtils swsUtils;
 
     public EventEntity getEventById(Long eventId) {
-        EventEntity event = eventRepository.findById(eventId).orElseThrow(RuntimeException::new);
-        return check(event);
-    }
+        EventEntity event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EventNotFoundException(
+                        String.format("Event with id: %s not found", eventId)
+                ));
 
-    public List<EventEntity> getEvents() {
-        return eventRepository.findAll().stream()
-                .map(this::check)
-                .collect(Collectors.toList());
-    }
-
-    public List<ResultEntity> getEventResultsById(Long eventId) {
-        EventEntity event = eventRepository.findById(eventId).orElseThrow(RuntimeException::new);
-        return check(event).getResults();
-    }
-
-    // todo rename it
-    private EventEntity check(EventEntity event) {
         if (!event.isFilled()) {
-            // scrap event from site
-            // save filled event
+            EventEntity filledEvent = swsUtils.fillEvent(event);
+            eventRepository.saveAndFlush(filledEvent);
         }
 
         return event;
+    }
+
+    public List<EventEntity> getEvents() {
+        List<EventEntity> events = eventRepository.findAll();
+        
+        if (events.isEmpty()) {
+            throw new EventNotFoundException(
+                    String.format("Events not found. Db probably empty")
+            );
+        }
+
+        List<EventEntity> filledEvents = events.parallelStream()
+                .filter(event -> !event.isFilled())
+                .map(swsUtils::fillEvent)
+                .collect(Collectors.toList());
+
+        eventRepository.saveAllAndFlush(filledEvents);
+
+        return events;
+    }
+
+    public List<ResultEntity> getEventResultsById(Long eventId) {
+        EventEntity event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EventNotFoundException(
+                        String.format("Event with id: %s not found", eventId)
+                ));
+
+        if (!event.isFilled() || event.getResults().isEmpty()) {
+            EventEntity filledEvent = swsUtils.fillEvent(event);
+            eventRepository.saveAndFlush(filledEvent);
+        }
+
+        List<RacerEntity> filledRacers = event.getRacers().parallelStream()
+                .filter(racer -> !racer.isFilled())
+                .map(swsUtils::fillRacer)
+                .collect(Collectors.toList());
+
+        racerRepository.saveAllAndFlush(filledRacers);
+
+        return event.getResults().stream()
+                .sorted(Comparator.comparingInt(ResultEntity::getPosition))
+                .collect(Collectors.toList());
     }
 }
