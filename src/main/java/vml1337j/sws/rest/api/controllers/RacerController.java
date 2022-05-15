c85303cfb132d2eeb9412f8069ec2e3951d2abab
vml1337j <vml1337j@yandex.ru>
2022-05-15 23:57:56 +0300

    impliment service & mapper

--- src/main/java/vml1337j/sws/rest/api/controllers/RacerController.java
+++ src/main/java/vml1337j/sws/rest/api/controllers/RacerController.java
@@ -1,13 +1,17 @@
 package vml1337j.sws.rest.api.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import vml1337j.sws.rest.api.dto.RacerDto;
 import vml1337j.sws.rest.api.dto.RacerEventDto;
+import vml1337j.sws.rest.api.mappers.RacerMapper;
+import vml1337j.sws.rest.api.services.RacerService;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class RacerController {
 
@@ -15,18 +19,27 @@ public class RacerController {
     public static final String GET_RACER = "/api/v1/racers/{racer_id}";
     public static final String FETCH_RACER_EVENTS = "/api/v1/racers/{racer_id}/events";
 
+    private final RacerService racerService;
+    private final RacerMapper racerMapper;
+
     @GetMapping(FETCH_RACERS)
     public List<RacerDto> fetchRacers() {
-        return List.of();
+        return racerMapper.toRacerDtoList(
+                racerService.getRacers()
+        );
     }
 
     @GetMapping(GET_RACER)
     public RacerDto getRacerById(@PathVariable("racer_id") Long racerId) {
-        return RacerDto.builder().build();
+        return racerMapper.toRacerDto(
+                racerService.getRacerById(racerId)
+        );
     }
 
     @GetMapping(FETCH_RACER_EVENTS)
     public List<RacerEventDto> fetchRacerEventsById(@PathVariable("racer_id") Long racerId) {
-        return List.of();
+        return racerMapper.toRacerResultDtoList(
+                racerService.getRacerResults(racerId)
+        );
     }
 }
