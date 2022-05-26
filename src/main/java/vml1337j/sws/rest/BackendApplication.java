7cde50c9388abbf110e6e50d2b8ecccd33f866c9
vml1337j <vml1337j@yandex.ru>
2022-05-26 10:54:45 +0300

    add swagger

--- src/main/java/vml1337j/sws/rest/BackendApplication.java
+++ src/main/java/vml1337j/sws/rest/BackendApplication.java
@@ -3,7 +3,9 @@ package vml1337j.sws.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
